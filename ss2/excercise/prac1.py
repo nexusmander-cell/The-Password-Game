fb799b0250a2fb72f00d87df18ec98be2cd45771
nexusmander-cell <nexusmander@gmail.com>
2025-11-23 16:32:53 +0700

    Delete ss2 directory

--- ss2/excercise/prac1.py
+++
@@ -1,9 +0,0 @@
-import re
-
-Pattern1 = r'\b[A-ZÀ-Ỹ][a-zà-ỹ]*\b'
-
-text = "Hôm nay trời Đẹp. Tôi sẽ Đi học Python ở Aptech."
-
-match = re.findall(Pattern1, text)
-
-print(match)
--- ss2/excercise/prac3.py
+++
@@ -1,9 +0,0 @@
-import re
-
-sample = "Hôm nay học #Python, mai học #Regex và #MachineLearning"
-
-pattern = r'#\w+'
-
-match = re.findall(pattern, sample)
-
-print(match)
